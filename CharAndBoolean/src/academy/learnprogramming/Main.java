d0b7fb10ca972982a51d5d163fd6fd38a463defc
marshallhelenm <marshallhelenm@gmail.com>
2019-12-19 18:45:28 -0800

    char and boolean

---
+++ CharAndBoolean/src/academy/learnprogramming/Main.java
@@ -0,0 +1,20 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        char myChar = 'D';
+        char myUnicodeChar = '\u0044';
+        System.out.println(myChar);
+        System.out.println(myUnicodeChar);
+        char myCopyrightChar = '\u00A9';
+        System.out.println(myCopyrightChar);
+        boolean myTrue = true;
+        boolean myFalse = false;
+
+        boolean isCustomerOverTwentyOne = true;
+
+
+    }
+}
