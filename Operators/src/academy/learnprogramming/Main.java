cda676443ca9aea0ed336e6f74b4ea367a5da50c
marshallhelenm <marshallhelenm@gmail.com>
2019-12-23 11:03:49 -0800

    if then

---
+++ Operators/src/academy/learnprogramming/Main.java
@@ -0,0 +1,12 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+	boolean isAlien = false;
+	if (isAlien == false) {
+        System.out.println("It is not an alien!");
+        System.out.println("And I am scared of aliens");
+    }
+    }
+}
