c089892474d8e94f670b77adf220bacd6be71e93
marshallhelenm <marshallhelenm@gmail.com>
2019-12-29 14:56:02 -0800

    doing some coding exercises. if thens and methods.

---
+++ Section4/CodingExercises/src/BarkingDog.java
@@ -0,0 +1,15 @@
+package PACKAGE_NAME;
+
+public class BarkingDog {
+    public static boolean shouldWakeUp(boolean barking, int hourOfDay){
+        if (hourOfDay < 0 || hourOfDay > 23){
+            return false;
+        };
+        if (barking && (hourOfDay < 8 || hourOfDay > 22)){
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+}
---
+++ Section4/CodingExercises/src/MegaBytesConverter.java
@@ -0,0 +1,12 @@
+public class MegaBytesConverter {
+    public static void printMegaBytesAndKiloBytes(int kiloBytes){
+        if (kiloBytes < 0){
+            System.out.println("Invalid Value");
+        } else {
+
+            int mb = Math.round(kiloBytes / 1024);
+            int remaining = kiloBytes % 1024;
+            System.out.println(kiloBytes + " KB = " + mb + " MB and " + remaining + " KB");
+        }
+    }
+}
