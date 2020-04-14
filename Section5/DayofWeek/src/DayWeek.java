711233b5e0b58b5d1edd3e8bc237692c634f3967
marshallhelenm <marshallhelenm@gmail.com>
2020-04-14 09:15:05 -0700

    completed day of week challenge

---
+++ Section5/DayofWeek/src/DayWeek.java
@@ -0,0 +1,34 @@
+public class DayWeek {
+
+    public static void main(String[] args){
+        printDayOfTheWeek(0);
+        printDayOfTheWeek(3);
+    }
+
+    public static void printDayOfTheWeek(int day){
+        switch(day){
+            case 0:
+                System.out.println("Sunday");
+                break;
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+
+        }
+    }
+}
